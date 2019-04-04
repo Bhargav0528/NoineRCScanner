2ccc4c2a2bfcb6fd69d3f3377718a353eaa642ef
bhargav0528 <bhargavauden1998@gmail.com>
2019-04-05 01:08:58 +0530

    App done

--- NoineRCApp/App.js
+++ NoineRCApp/App.js
@@ -1,16 +1,46 @@
+
 import React, {Component} from 'react';
 import {Text, View, Image, StatusBar} from 'react-native';
-import PlateDetector from './src/components/RCScan';
+import RCScan from './src/components/RCScan';
 import firebase from 'firebase';
 
 export default class App extends Component{
 
+  state = { splash: true };
+
+  componentWillMount(){
+    // Initialize Firebase
+    setTimeout(() => this.setState({ splash: false }), 3000);
+    if (!firebase.apps.length) {
+      const config = {
+        apiKey: "AIzaSyDBa5IOIdomv1I8Fi8LCw9ueSsg2_S_doE",
+        authDomain: "malpr-4c337.firebaseapp.com",
+        databaseURL: "https://malpr-4c337.firebaseio.com",
+        projectId: "malpr-4c337",
+        storageBucket: "malpr-4c337.appspot.com",
+        messagingSenderId: "384049723713"
+      };
+      firebase.initializeApp(config);
+    }
+  }
 
   render() {
+   if (this.state.splash) {
+     return (
+          <View style={{ flex: 1, backgroundColor: '#272727', justifyContent: 'center', alignItems: 'center' }} >
+            <StatusBar hidden />
+            <View style={{ borderRadius: 100, backgroundColor: 'orange', height: '40%', width: '20%', justifyContent: 'center', alignItems: 'center' }} >
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>RCScanner</Text>
+            </View>
+          </View>
+         //<Image source={require('./src/resources/splash.png')} style={{ flex: 1 }} />
+     );
+   }
    return (
      <View style={{flex:1}}>
-      <Text>sda</Text>
+       <StatusBar hidden />
+       <RCScan />
      </View>
    );
-   }
+ }
 }
--- ocr.py
+++ ocr.py
@@ -4,14 +4,22 @@ import cv2
 import os
 import sys
 import numpy as np
+from cv2 import boundingRect, countNonZero, cvtColor, drawContours, findContours, getStructuringElement, imread, morphologyEx, pyrDown, rectangle, threshold
 
 def imgTest(image):
+
+        #rgb = pyrDown(image)
+        
+        # apply grayscale
+        #preimage = cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+        
         preimage = cv2.fastNlMeansDenoisingColored(image,None,20,20,7,20)
         #cv2.imwrite('noise.jpg', preimage)
         preimage = cv2.cvtColor(preimage, cv2.COLOR_BGR2GRAY)
         preimage = cv2.adaptiveThreshold(preimage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 3)
         #preimage = cv2.threshold(preimage, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         preimage = cv2.medianBlur(preimage, 3)
+        
         filename = "{}.png".format(os.getpid())
         cv2.imwrite(filename, preimage)
 
@@ -24,6 +32,10 @@ def main():
         h, w = testImage.shape[:2]
         ratio = w/h
         
+        
+        print(imgTest(testImage))
+        
+
         for width in range(1700,2000,25):
                 height = int(width / ratio)
                 image = cv2.resize(testImage, (width, height))
@@ -34,4 +46,6 @@ def main():
                 except UnicodeEncodeError:
                         print('UnicodeError')
 
+        cv2.imshow("sd",testImage)
+        cv2.waitKey(0)
 main()
