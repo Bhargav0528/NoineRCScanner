d0b1910febd6845f5ec2d0292263a9cf2e8840f5
anvitha1AA <abhi1aa@outlook.com>
2019-04-04 19:22:26 +0530

    clean_text.py updated for Andhra RCs

--- Text_process/clean_text.py
+++ Text_process/clean_text.py
@@ -1,7 +1,7 @@
 import re
 
 
-str = """ANDHRA PRADESH TRANSPORT DEPARTMENT
+str = """State: ANDHRA PRADESH TRANSPORT DEPARTMENT
 CERTIFICATE OF REGISTRATION
 
  
@@ -29,6 +29,31 @@ Fuel Used PETROL
 Type of Body : 2 WHEELER
 """
 
+str1 = """ 
+Reg No. : AP16CF9628 '
+
+1 7K RAVI VARMA _ - m
+NAGESWARA RAO =. ‘
+
+:119812 PLOT NO 306_ 5
+
+13:17:)“; GAYA'lgRl RES BLOCK 3
+
+’Regn; NUmber . '-
+Regd. Owgg
+
+
+K ‘ 'lg_~;MAn\g ROAD KANURU
+A \_ Maker sic1ass ?:i'PL'Jis’AR 150 01's.: BSIII
+>: , ' " \- Vehicle cé‘s‘s; ‘ ”MOTOR CYCLE
+7 O Mth. Yr. of mg“? “ £2013 \ ;
+
+‘lFuelUsed : PETROL
+Type ofBoqv = :S'QLOX
+
+
+"""
+
 # print(str)
 
 str = "".join([s for s in str.strip().splitlines(True) if s.strip()])
@@ -37,10 +62,84 @@ str = re.sub('[^a-zA-Z0-9\\n\\s:\']', '', str)
 str = re.sub('\s[a-z]\s', '\n', str)
 
 
+data = dict()
+j=0;
 
+keys = []
+info = []
 str = str.split(':')
+print(str)
+prvStr = re.match(r'\b[A-Z]+[a-z]+\b', str[0])
+k_str = prvStr.group()
+ij = re.sub(r'\b[A-Z]+[a-z]+\b', '', str[1])
+print(ij)
+prvStr_i = re.match(r'[A-Z\\n0-9\s]+', ij)
+i_str = prvStr_i.group()
 for s in str:
-    s = s.split(' ')
-    for i in s:
-        print(i)
-        print(re.match('[A-Z\\n0-9]+', i))
+    final_val = ""
+    s = re.sub('\\n',' ', s)
+    # s = s.strip().split(' ')
+    # print(s)
+    s =s.strip().split()
+    for index, i in enumerate(s):
+        key = re.match(r'(\b[A-Z]+[a-z]+\b)|(\b[a-z]+\b)', i)
+        if( key!= None and prvStr != None):
+            k_str = k_str + " " + key.group()
+ 
+        else:
+            keys.append(k_str)
+            k_str = ' '
+
+    # s = re.sub('\\n',' ', s)
+    # # s = s.strip().split(' ')
+    # # print(s)
+    # s =s.strip().split()
+    for index, i in enumerate(s):
+        i = re.sub(r'\b[A-Z]+[a-z]+\b', '', i)
+        value = re.match(r'\b[A-Z\\n0-9]+\b', i)
+        print(value)
+        if( value!= None and prvStr_i != None):
+            i_str = i_str + " " + value.group()
+        elif(index==0):
+            i_str = ''
+        else:
+            info.append(i_str)
+            i_str = ' '
+   
+
+
+    
+    # if j==0:
+    #     previousKey = re.match('[A-Z]+[a-z]+', s[0])
+    #     j =1;
+    # for i in s:
+    #     print('p',previousKey)
+    #     print('i', i)
+    #     key = re.match('[A-Z]+[a-z]+', i)
+    #     print('k', key)
+        
+    #     i = re.sub('[A-Z]+[a-z]+', '', i)
+    #     value = re.match('[A-Z\\n0-9]+', i)
+    #     print('val',value)
+        
+    #     if(key != previousKey and key!=None):
+    #         data[previousKey.group()] = final_val
+    #         final_val = " "
+    #     elif(value!= None):
+    #         final_val = final_val + " " + value.group()
+    #         print(final_val)
+
+    #     if(key!=None):
+    #         previousKey = key
+
+info.append(i_str)
+keys = [x for x in keys if x != ' ']
+print(keys)
+
+info = [x for x in info if x != ' ']
+print(info)
+
+for key, d in zip(keys, info):
+    data[key] = d
+
+print(data)
