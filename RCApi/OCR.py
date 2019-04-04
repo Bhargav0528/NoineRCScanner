82583dcea03044253ed093acdd0c6d10783a9793
mehulpoddar <mehulpoddar@gmail.com>
2019-04-04 18:38:35 +0530

    correct ocr

--- RCApi/OCR.py
+++ RCApi/OCR.py
@@ -1,41 +1,35 @@
-# USAGE
-# python ocr.py --image images/example_01.png 
-# python ocr.py --image images/example_02.png  --preprocess blur
-
-# import the necessary packages
 from PIL import Image
 import pytesseract
-import argparse
 import cv2
 import os
 import sys
+import numpy as np
 
 def imgTest(image):
-        preimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        preimage = cv2.threshold(preimage, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        preimage = cv2.fastNlMeansDenoisingColored(image,None,10,10,7,20)
+        #cv2.imwrite('noise.jpg', preimage)
+        preimage = cv2.cvtColor(preimage, cv2.COLOR_BGR2GRAY)
+        preimage = cv2.adaptiveThreshold(preimage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 1)
+        #preimage = cv2.threshold(preimage, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         preimage = cv2.medianBlur(preimage, 3)
-
-        # write the grayscale image to disk as a temporary file so we can
-        # apply OCR to it
         filename = "{}.png".format(os.getpid())
         cv2.imwrite(filename, preimage)
 
-        # load the image as a PIL/Pillow image, apply OCR, and then delete
-        # the temporary file
         text = pytesseract.image_to_string(Image.open(filename))
-        os.remove(filename)
+        #os.remove(filename)
         return text
 
-def readRC(testImage):     
-        #testImage = cv2.imread('images/'+sys.argv[1])
+def main():     
+        testImage = cv2.imread('images/'+sys.argv[1])
         h, w = testImage.shape[:2]
         ratio = w/h
         
-        for width in range(25,2701,25):
+        for width in range(1700,2000,25):
                 height = int(width / ratio)
                 image = cv2.resize(testImage, (width, height))
                 try:
                         print(width,'x',height,':',imgTest(image))
                 except UnicodeEncodeError:
                         print('UnicodeError')
+
+main()
